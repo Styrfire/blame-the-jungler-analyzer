03bcc6fc803c24a77ae51b21f9739e017b2d7990
Styrfire <g_n_rogers1991@yahoo.com>
2020-07-28 15:13:47 -0500

    Built out base controller, added riot api, and added server

--- src/main/groovy/com/analyzer/web/controller/BlameTheJunglerController.java
+++ src/main/groovy/com/analyzer/web/controller/BlameTheJunglerController.java
@@ -1,10 +1,31 @@
 package com.analyzer.web.controller;
 
+import com.riot.api.RiotApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class BlameTheJunglerController
 {
+	private RiotApi api;
+
+	private static Logger logger = LoggerFactory.getLogger(BlameTheJunglerController.class);
+
+	@Inject
+	BlameTheJunglerController()
+	{
+		this.api = new RiotApi(System.getProperty("api.key"));
+	}
+
+	@RequestMapping("/")
+	String helloWorld()
+	{
+		return "Hello world from the Blame the Jungler Analyzer!";
+	}
 }
