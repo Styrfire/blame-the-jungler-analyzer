242e3763e0a000fcbae4e7324cad3a50a1a549e2
Styrfire <g_n_rogers1991@yahoo.com>
2020-07-28 11:48:40 -0500

    Initialize repo

---
+++ src/main/groovy/com/analyzer/Server.java
@@ -0,0 +1,15 @@
+package com.analyzer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Server extends SpringBootServletInitializer
+{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder)
+	{
+		return applicationBuilder.sources(Server.class);
+	}
+}
---
+++ src/main/groovy/com/analyzer/web/controller/BlameTheJunglerController.java
@@ -0,0 +1,10 @@
+package com.analyzer.web.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class BlameTheJunglerController
+{
+}
